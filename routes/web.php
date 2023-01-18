97233748d917ab22cd4852bacc6207bab4df1216
Ngoctu1 <92847247+Ngoctu1@users.noreply.github.com>
2023-01-19 00:14:50 +0700

    Revert "."

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,22 @@ use Illuminate\Support\Facades\Route;
 use PhpParser\Node\Stmt\Function_;
 
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 //--------------login-------------------
 //Route::get('/login', [loginController::class,'getlogin']) -> name('users.login');
 //Route::post('/login', [loginController::class,'login']) -> name('users.login');
+
+
 
 
 //---------------User----------------
